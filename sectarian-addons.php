5cd7dfde519059a8f3467d18a9453c18ac70aa1d
makercode <makercode@gmail.com>
2023-12-29 17:47:57 -0500

    fix: renaming hooks to prevent repeated names

--- sectarian-addons.php
+++ sectarian-addons.php
@@ -2,27 +2,27 @@
 /**
  * Plugin Name: Sectarian Addons
  * Description: Sectarian addons widgets for Elementor.
- * Version:     1.0.3
+ * Version:     1.0.4
  * Author:      Elementor Developer
  * Author URI:  https://codesectarian.com/
  * Text Domain: codesectarian
  */
 
 
-function register_widget_styles() {
+function sectaddons_register_widget_styles() {
 	wp_register_style( 'widget-banner-styles', plugins_url( 'public/src/css/widget-banner-styles.css', __FILE__ ) );
 	wp_register_style( 'widget-timeline-styles', plugins_url( 'public/src/css/widget-timeline-styles.css', __FILE__ ) );
 }
-add_action( 'wp_enqueue_scripts', 'register_widget_styles' );
+add_action( 'wp_enqueue_scripts', 'sectaddons_register_widget_styles' );
 
 
-function register_widget_scripts() {
+function sectaddons_register_widget_scripts() {
     wp_register_script( 'widget-banner-scripts', 'https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js' );
 }
-add_action( 'wp_enqueue_scripts', 'register_widget_scripts' );
+add_action( 'wp_enqueue_scripts', 'sectaddons_register_widget_scripts' );
 
 
-function register_sectarian_widget( $widgets_manager ) {
+function sectaddons_register_sectarian_widget( $widgets_manager ) {
 
 	require_once( __DIR__ . '/public/widgets/banner-parallax.php' );
 	require_once( __DIR__ . '/public/widgets/timeline.php' );
@@ -31,5 +31,5 @@ function register_sectarian_widget( $widgets_manager ) {
 	$widgets_manager->register( new \Elementor_Timeline() );
 
 }
-add_action( 'elementor/widgets/register', 'register_sectarian_widget' );
+add_action( 'elementor/widgets/register', 'sectaddons_register_sectarian_widget' );
 
